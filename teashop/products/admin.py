f5bf1043351c7d368e9a5ebdae84b3427c21f8b5
Synthetic-Ninja <sinyagin.timofey@yandex.ru>
2023-02-28 12:51:56 +0300

    Add 'ProductCategories', 'Brands', 'Products' models and register them in admin page. Add first product and media folder

--- teashop/products/admin.py
+++ teashop/products/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Products,ProductCategories,Brands
+
+
+@admin.register(Products)
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'discount', 'quantity')
+    fields = ('is_available', 'name', 'description',
+              'price', 'discount', 'quantity',
+              'image', 'category', 'brand')
+
+
+@admin.register(ProductCategories)
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    fields = ('name', 'description')
+
+
+@admin.register(Brands)
+class BrandsAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    fields = ('name', 'description')
+
+
+
--- teashop/products/models.py
+++ teashop/products/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
-# Create your models here.
+
+class ProductCategories(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=128, unique=True)
+    description = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return f'Категория: {self.id} | Название: [{self.name}]'
+
+
+class Brands(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=128, unique=True)
+    description = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Бренд'
+        verbose_name_plural = 'Бренды'
+
+    def __str__(self):
+        return f'Бренд: {self.id} | Название: {self.name}'
+
+
+class Products(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    is_available = models.BooleanField()
+    name = models.CharField(max_length=128, unique=True)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=11, decimal_places=2)
+    discount = models.PositiveBigIntegerField(default=0, validators=[MaxValueValidator(100)])
+    quantity = models.PositiveBigIntegerField()
+    image = models.ImageField(upload_to='products_images')
+    category = models.ForeignKey(to=ProductCategories, on_delete=models.CASCADE)
+    brand = models.ForeignKey(to=Brands, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+
+    def __str__(self):
+        return f'Продукт: {self.name} | Категория: {self.category.name} | Бренд {self.brand.name}'
