581e3ce1f6eca41ed2d49c5c9b53902bdae69378
Synthetic-Ninja <sinyagin.timofey@yandex.ru>
2023-03-03 18:27:53 +0300

    Rebuild products/views to class based views
    - Add ProductListView to product/views
    - Add ProductFilteredListView to product/view
    - Add views in product/urls

--- teashop/products/urls.py
+++ teashop/products/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import products_list
+from .views import ProductsListView, ProductFilteredListView
 
 app_name = 'products'
 
-urlpatterns = [
-    path('', products_list, name='product_list'),
 
+urlpatterns = [
+    path('', ProductsListView.as_view(), name='product_list'),
+    path('filter/', ProductFilteredListView.as_view(), name='filtered_product_list')
 ]
--- teashop/products/views.py
+++ teashop/products/views.py
@@ -1,14 +1,42 @@
-from django.shortcuts import render
+import re
+from decimal import Decimal
+
+from django.views.generic import ListView
 
 from .models import Products, ProductCategories, Brands
 
 
-def products_list(request):
+class ProductsListView(ListView):
+    """Class returns productlist template with all available products"""
 
-    context = {'products': Products.objects.filter(is_available=True),
-               'product_categories': ProductCategories.objects.all(),
-               'brands': Brands.objects.filter(),
-               'title': 'Products'}
+    model = Products
+    template_name = 'products/productlist.html'
 
-    return render(request, 'products/productlist.html', context=context)
+    def get_queryset(self):
+        return Products.objects.filter(is_available=True)
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data()
+        context.update({
+                'product_categories': ProductCategories.objects.all(),
+                'brands': Brands.objects.filter(),
+                'title': 'Products'}
+        )
+        return context
+
+
+class ProductFilteredListView(ProductsListView):
+    """Class returns productlist template with filtered products"""
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        if (brands := self.request.GET.getlist('brands')) is not None:
+            queryset = queryset.filter(brand__id__in=brands)
+
+        if (price_filter_param := self.request.GET.get('price')) is not None:
+            parsed_prices = re.findall(r'\$\d{1,4}', price_filter_param)
+            if len(parsed_prices) == 2:
+                min_price, max_price = (Decimal(price[1:]) for price in parsed_prices)
+                queryset = queryset.filter(price__gte=min_price, price__lte=max_price)
+
+        return queryset
