b2b0f4bc1c73e41e87916c1fe59c311b0c784054
Synthetic-Ninja <sinyagin.timofey@yandex.ru>
2023-06-28 12:34:39 +0300

    add users urls to teashop/urls.py

--- teashop/teashop/urls.py
+++ teashop/teashop/urls.py
@@ -23,6 +23,7 @@ from index.views import index
 urlpatterns = [
     path('', index, name='index'),
     path('products/', include('products.urls', namespace='products')),
+    path('users/', include('users.urls', namespace='users')),
     path('admin/', admin.site.urls)
 ]
 
