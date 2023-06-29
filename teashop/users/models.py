7fbef7e74f8ca941089309e8e4ef0636d0f7c20a
Synthetic-Ninja <sinyagin.timofey@yandex.ru>
2023-06-29 18:17:29 +0300

    Add User model

--- teashop/users/models.py
+++ teashop/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from uuid import uuid4
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    id = models.UUIDField(default=uuid4, primary_key=True)
+    image = models.ImageField(upload_to='users_image', null=True, blank=True)
+    email = models.EmailField(unique=True, blank=False, null=False)
+    is_verified_email = models.BooleanField(default=False)
+
